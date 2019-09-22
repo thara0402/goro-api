22e71ebb8705d4a08e1a09a91d1777f0c0c97928
thara0402 <thara0402@gmail.com>
2019-09-22 21:25:41 +0900

    update app insight.

--- Goro.Api/Controllers/GourmetController.cs
+++ Goro.Api/Controllers/GourmetController.cs
@@ -48,27 +48,27 @@ namespace Goro.Api.Controllers
             return Ok(result);
         }
 
-        /// <summary>
-        /// 
-        /// </summary>
-        /// <param name="keyword"></param>
-        /// <returns></returns>
-        /// <response code="200"></response>
-        /// <response code="404"></response>
-        [HttpGet("{keyword}")]
-        [ProducesResponseType(typeof(IEnumerable<Gourmet>), 200)]
-        [ProducesResponseType(404)]
-        public async Task<IActionResult> Get(string keyword)
-        {
-			var geocode = await GeocodeClient.GetGeocodeAsync(HttpUtility.UrlDecode(keyword));
-			var entity = await GourmetClient.SearchAsync(geocode.results[0].geometry.location.lng, geocode.results[0].geometry.location.lat);
-            var result = _mapper.Map<List<Gourmet>>(entity);
-            if (result == null || result.Count == 0)
-            {
-                return NotFound();
-            }
-            return Ok(result);
-        }
+   //     /// <summary>
+   //     /// 
+   //     /// </summary>
+   //     /// <param name="keyword"></param>
+   //     /// <returns></returns>
+   //     /// <response code="200"></response>
+   //     /// <response code="404"></response>
+   //     [HttpGet("{keyword}")]
+   //     [ProducesResponseType(typeof(IEnumerable<Gourmet>), 200)]
+   //     [ProducesResponseType(404)]
+   //     public async Task<IActionResult> Get(string keyword)
+   //     {
+			//var geocode = await GeocodeClient.GetGeocodeAsync(HttpUtility.UrlDecode(keyword));
+			//var entity = await GourmetClient.SearchAsync(geocode.results[0].geometry.location.lng, geocode.results[0].geometry.location.lat);
+   //         var result = _mapper.Map<List<Gourmet>>(entity);
+   //         if (result == null || result.Count == 0)
+   //         {
+   //             return NotFound();
+   //         }
+   //         return Ok(result);
+   //     }
 
        /// <summary>
         /// 
