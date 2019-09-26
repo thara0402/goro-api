affc825d33a28687998296ef7033fbda31f3c6a3
Toshiyuki Hara <thara@archway.co.jp>
2019-09-26 19:19:17 +0900

    update configration with key vault.

--- Goro.Api/Program.cs
+++ Goro.Api/Program.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Configuration.AzureAppConfiguration;
 using Microsoft.Extensions.Logging;
 
 namespace Goro.Api
@@ -19,6 +20,15 @@ namespace Goro.Api
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
+                .ConfigureAppConfiguration(config =>
+                {
+                    var settings = config.Build();
+
+                    //config.AddAzureAppConfiguration(options =>
+                    //    options.ConnectWithManagedIdentity(settings["AppConfig:Endpoint"]));
+
+                    config.AddAzureKeyVault(settings["KeyVault:Endpoint"]);
+                })
                 .UseApplicationInsights()
                 .UseStartup<Startup>();
     }
